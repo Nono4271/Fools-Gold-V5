ff7a68c89b764c1532a57dcd344c98207c15d835
Nono4271 <testnovaserver2@gmail.com>
2026-05-15 13:32:13 -0400

    Update MapRenderer.jsx

--- src/MapRenderer.jsx
+++ src/MapRenderer.jsx
@@ -1544,8 +1544,8 @@ function _buildOneHQ(tileKey, tile, selKey, onHQClick, PIXI, isPanningRef, texCa
     sp.x = spriteX;
     sp.y = spriteY;
 
-    sp.rotation = 0.09;
-    sp.skew.x = -0.19;
+    sp.rotation = -0.19;
+    sp.skew.x = 0;
     // sp.skew.y = 0;
   };
 
