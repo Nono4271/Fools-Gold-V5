901bb1bb9900c6edb71f1d3fd6975bdf35dcdc49
Nono4271 <testnovaserver2@gmail.com>
2026-05-15 08:35:22 -0400

    Add files via upload

--- src/MapRenderer.jsx
+++ src/MapRenderer.jsx
@@ -1528,7 +1528,7 @@ function _buildOneHQ(tileKey, tile, selKey, onHQClick, PIXI, isPanningRef, texCa
   const targetH = targetW * 0.90;
 
   const spriteX = bx - TW * 0.25;
-  const spriteY = worldCY - elev;
+  const spriteY = worldCY - elev - TH * 1.5;
 
   const applySprite = (sp) => {
     sp.anchor.set(0.5, 0.68);
