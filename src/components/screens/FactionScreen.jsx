65dbc6b7300b500a1793bde66473151c8a9a4285
Nono4271 <testnovaserver2@gmail.com>
2026-05-22 22:15:47 -0400

    Add files via upload

--- src/components/screens/FactionScreen.jsx
+++ src/components/screens/FactionScreen.jsx
@@ -142,7 +142,7 @@ export default function FactionScreen({
     }
 
     setFacKey(f.key);
-    setFacName(f.n);
+    // setFacName(f.n); // Don't overwrite player name
     setAiRss({stone:300,wood:300,ore:300,gas:300});
     setAiBldgs({hq:1,quarry:0,lumber:0,forge:0,refinery:0,barracks:0,training:0,commandcenter:0,healingtent:0,walls:0});
     setAiBarracksPool(barracksCapacity(0));
