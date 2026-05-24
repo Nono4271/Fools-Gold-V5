08d1ee7bfb75decb625fc195453bffa0555e4437
Nono4271 <testnovaserver2@gmail.com>
2026-05-23 21:59:03 -0400

    Add files via upload

--- shared/constants/troops.js
+++ shared/constants/troops.js
@@ -788,7 +788,7 @@ export const FACTION_TROOPS = {
   },
 
   // ── ASHEN DEAD ───────────────────────────────────────────────────────────────
-  ashen_dead: {
+  ashen dead: {
     quarters: "The Necropolis",
     branches: [
       {
