d8775fc3298c670d3fcd4c5507a8fbc732aa2b68
Nono4271 <testnovaserver2@gmail.com>
2026-05-25 23:35:27 -0400

    Add files via upload

--- src/workers/mapGen.worker.js
+++ src/workers/mapGen.worker.js
@@ -378,17 +378,17 @@ function buildBordersFromCrossings(REGION_MAP, CROSSINGS) {
     const { axis, bCoord, gCoord, type, id } = crossing;
     
     if (axis === 'H') {
-      // Horizontal border at y = bCoord, gate at x = gCoord
-      // 4-tile gap: gCoord-2, gCoord-1, gCoord, gCoord+1
-      // Layout: Gate A at gCoord-2, Path at gCoord-1, Path at gCoord, Gate B at gCoord+1
-      gateA.push({ x: gCoord - 2, y: bCoord, id: id+'_A', type, axis });
-      gateB.push({ x: gCoord + 1, y: bCoord, id: id+'_B', type, axis });
-    } else {
-      // Vertical border at x = bCoord, gate at y = gCoord
+      // Horizontal border at y = bCoord, gate spans vertically to cross perpendicular
       // 4-tile gap: gCoord-2, gCoord-1, gCoord, gCoord+1
       // Layout: Gate A at gCoord-2, Path at gCoord-1, Path at gCoord, Gate B at gCoord+1
       gateA.push({ x: bCoord, y: gCoord - 2, id: id+'_A', type, axis });
       gateB.push({ x: bCoord, y: gCoord + 1, id: id+'_B', type, axis });
+    } else {
+      // Vertical border at x = bCoord, gate spans horizontally to cross perpendicular
+      // 4-tile gap: gCoord-2, gCoord-1, gCoord, gCoord+1
+      // Layout: Gate A at gCoord-2, Path at gCoord-1, Path at gCoord, Gate B at gCoord+1
+      gateA.push({ x: gCoord - 2, y: bCoord, id: id+'_A', type, axis });
+      gateB.push({ x: gCoord + 1, y: bCoord, id: id+'_B', type, axis });
     }
   }
   
