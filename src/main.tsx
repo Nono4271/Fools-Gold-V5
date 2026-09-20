9ef7a7190e538655b8df1678f6012ca0d9370504
Nono4271 <testnovaserver2@gmail.com>
2026-09-20 17:57:21 -0400

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -18,6 +18,7 @@ document.addEventListener(
     if ((e.target as Element | null)?.closest(".roster-scroll")) return;
     if ((e.target as Element | null)?.closest(".battle-popup")) return;
     if ((e.target as Element | null)?.closest(".find-tiles-popup")) return;
+    if ((e.target as Element | null)?.closest(".chat-scroll")) return;
     const target = e.target as Element | null;
     if (!target) return;
     const interactive = target.closest(
